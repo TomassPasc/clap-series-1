5fa8c26a0250892aa3aa5b62f806de8f370ab0e2
ThomasPascual <thoma1@free.fr>
2020-06-09 00:02:49 +0200

    dealer done

--- src/dealer.js
+++ src/dealer.js
@@ -10,8 +10,35 @@
  * - Le type de la carte (ordre croissant) : d => ♦, c => ♣, h => ♥, s => ♠
  * 
  */
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 function dealer() {
-  // CODE HERE
+  const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const cardColor = ["d", "c", "h", "s"];
+  const decks = [];
+  const result = [];
+
+  //création du decks
+  cardValue.forEach(function(value){
+  	 cardColor.forEach(function(color){
+     decks.push(value + color);
+    	})
+    })
+
+    //shuffle up and deal
+    const decksShuffle = shuffle(decks);
+    const hand1 = decksShuffle.slice(0,5);
+    const hand2 = decksShuffle.slice(5,10);
+    result.push(hand1);
+    result.push(hand2);
+    return result;
 }
 
 export { dealer };
